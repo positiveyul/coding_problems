f444230f0df4c8600fc3b6eff33c9037ac304329
positiveyul <88725167+positiveyul@users.noreply.github.com>
2021-09-25 22:08:58 +0900

    Add files via upload
    code up 기초 python 100제 풀이중.....1

---
+++ codeup_6001~.py
@@ -0,0 +1,68 @@
+#       6001
+print("Hello")
+
+# 6002
+print("Hello "+"World")
+
+# 6003
+print("Hello\nWorld")
+
+# 6004
+print("""'Hello'""")
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print("""\"!@#$%^&*()\'""")
+
+# 6007
+print("""\"C:\\Download\\\'hello\'.py\"""")
+
+# 6008
+print("""print(\"Hello\\nWorld\")""")
+
+# 6009
+# print(input())
+
+# 6010
+# print(int(input()))
+
+# 6011
+# print(float(input()))
+
+# 6012
+# a= input()
+# b= input()
+# print(a)
+# print(b)
+
+# 6013
+# a = []
+# a.append(input())
+# a.append(input())
+
+# print(a.pop())
+# print(a.pop())
+
+# # 6014
+# a = input()
+# print((a+"\n")*3)
+
+# 6015
+# a,b = input().split()
+# print(a+"\n"+b)
+
+# 6016
+
+# li = list(input().split())
+# print(li.pop())
+# print(li.pop())
+
+# 6017
+# a = input()
+# print(a,a,a)
+
+# 6018
+# a, b = input().split(':')
+# print(a, b, sep=(":"))
